29fd63e3876f2528d84643c1760dc6929fcf078b
Administrator <admin@example.com>
2016-08-23 09:50:41 -0500

    remove change run command

--- manifests/user/service.pp
+++ manifests/user/service.pp
@@ -18,26 +18,29 @@ define wso2::user::service  (
   #  down        => true,
   #  timestamp   => false,
   #}
-  file { "${basedir}/runit/${product}/run":
+  file { "${basedir}/${product}/run":
     ensure  => present,
     mode    => '0555',
     owner   => $user,
     group   => $group,
     content => template("wso2/${product}/run.erb"),
-    require => File["${basedir}/runit/${product}"],
   }
-  file { "${basedir}/service/${product}":
-    ensure  => link,
-    target  => "${basedir}/runit/${product}",
-    owner   => $user,
-    group   => $group,
-    replace => false,
-    require => File["${basedir}/runit/${product}/run"],
+  exec { 'start app':
+    command => "${basedir}/${product}/run",
+    path    => ['/bin', '/usr/local/bin', '/usr/bin', '/usr/local/sbin','/sbin','/usr/sbin'],
   }
-  file { "${logdir}/repository":
-    ensure  => link,
-    owner   => $user,
-    target  => "${basedir}/product/${product}-${version}/repository/logs",
-    require => File[$logdir],
-  }
+  # file { "${basedir}/service/${product}":
+  #  ensure  => link,
+  #  target  => "${basedir}/runit/${product}",
+  #  owner   => $user,
+  #  group   => $group,
+  #  replace => false,
+  #  require => File["${basedir}/runit/${product}/run"],
+  #  }
+  #file { "${logdir}/repository":
+  #  ensure  => link,
+  #  owner   => $user,
+  #  target  => "${basedir}/product/${product}-${version}/repository/logs",
+  #  require => File[$logdir],
+  #}
 }
